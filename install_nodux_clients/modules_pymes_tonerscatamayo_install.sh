c1b75561fec16db1edb0143a586815d45fc1908c
Fabi?n Calle <fabianc7@gmail.com>
2016-04-30 10:50:27 -0500

    Updated installed modules on Toners Catamayo

--- install_nodux_clients/modules_pymes_tonerscatamayo_install.sh
+++ install_nodux_clients/modules_pymes_tonerscatamayo_install.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Nodux: Needed modules for Toners Catamayo with Electronic Invoice
+# Nodux: Needed modules for Toners Catamayo
 
-# Modulos Core: 24 modulos
+# Modulos Core: 19 modules
 modulos_core=(
     'account' \
-    'account_credit_limit' \
     'account_invoice' \
-    'account_invoice_history' \
+    #'account_invoice_history' \
     'account_invoice_stock' \
     'account_product' \
     'account_statement' \
     'bank' \
-    'carrier' \
+    #'carrier' \
     'company' \
     'country' \
     'currency' \
-    'dashboard' \
+    #'dashboard' \
     'party' \
     'product' \
     'product_cost_history' \
     'product_price_list' \
     'purchase' \
     'sale' \
-    'sale_credit_limit' \
     'sale_invoice_grouping' \
     'sale_price_list' \
     'stock' \
@@ -36,13 +34,19 @@ echo 'Cloning "'$i'" module ...'
     hg clone -b 3.4 https://bitbucket.org/tryton/$i $i
 done
 
-# Modulos Nodux: 6 modulos
+# Modulos Nodux: 10 modules
 modulos_nodux=(
     'nodux_account_ec_pymes' \
-    'nodux_account_electronic_invoice_ec' \
+    #'nodux_account_electronic_invoice_ec' \
+    'nodux_account_invoice_multisequence' \
     'nodux_account_niif_ec' \
+    'nodux_account_statement' \
+    'nodux_account_voucher_ec' \
+    'nodux_party_ec' \
     'nodux_sale_payment' \
-    'nodux_sale_pos_electronic_invoice_ec' \
+    'nodux_sale_pos' \
+    'nodux_sale_pos_discount' \
+    #'nodux_sale_pos_electronic_invoice_ec' \
     'nodux_stock_remission_guide')
 
 for i in "${modulos_nodux[@]}"
@@ -52,8 +56,11 @@ echo 'Cloning "'$i'" module ...'
     hg clone https://bitbucket.org/nodux/$i $i
 done
 
-# Modulos Trytonspain: 1 modulo
-modulos_trytonspain=('sale_payment')
+# Modulos Trytonspain: 3 modules
+modulos_trytonspain=(
+    'account_invoice_discount' \
+    'sale_discount' \
+    'sale_payment')
 
 for i in "${modulos_trytonspain[@]}"
 do
@@ -62,9 +69,10 @@ echo 'Cloning "'$i'" module ...'
     hg clone -b 3.4 https://bitbucket.org/trytonspain/trytond-$i $i
 done
 
-# Modulos Zikzakmedia: 4 modulos
+# Modulos Zikzakmedia: 5 modules
 modulos_zikzakmedia=(
     'sale_pos' \
+    'sale_pos_discount' \
     'sale_product_stock' \
     'sale_product_stock_shop' \
     'sale_shop')
